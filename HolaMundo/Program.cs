ab8d363e2d322fd96090a1289dfe21fe72ed2a66
Portatil <Portatil@DESKTOP-EH59PCK>
2022-06-10 13:40:06 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecucion hasta presionar cualquier tecla
+        }
+    }
+}
